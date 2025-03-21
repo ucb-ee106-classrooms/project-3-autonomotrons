12b0079c08f8460c23c0679e4f30495cdf659118
daniel <danielmunicio@berkeley.edu>
2025-03-21 16:56:55 -0700

    Turtlebot code removed comments

--- src/turtlebot_proj3_pkg/src/turtlebot_ekf.py
+++ src/turtlebot_proj3_pkg/src/turtlebot_ekf.py
@@ -1,26 +1,3 @@
-# u : list
-#     A list of system inputs, where, for the ith data point u[i],
-#     u[i][0] is timestamp (s),
-#     u[i][1] is left wheel rotational speed (rad/s), and
-#     u[i][2] is right wheel rotational speed (rad/s).
-# x : list
-#     A list of system states, where, for the ith data point x[i],
-#     x[i][0] is timestamp (s),
-#     x[i][1] is bearing (rad),
-#     x[i][2] is translational position in x (m),
-#     x[i][3] is translational position in y (m),
-#     x[i][4] is left wheel rotational position (rad), and
-#     x[i][5] is right wheel rotational position (rad).
-# y : list
-#     A list of system outputs, where, for the ith data point y[i],
-#     y[i][0] is timestamp (s),
-#     y[i][1] is translational position in x (m) when freeze_bearing:=true,
-#     y[i][1] is distance to the landmark (m) when freeze_bearing:=false,
-#     y[i][2] is translational position in y (m) when freeze_bearing:=true, and
-#     y[i][2] is relative bearing (rad) w.r.t. the landmark when
-#     freeze_bearing:=false.
-
-# we need to convert cmd vel messages to left and right angular velocity for some reason
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
@@ -55,7 +32,6 @@ class turtlebot_ekf():
         qZ = msg.pose.pose.orientation.z
         phi = self.quat_to_yaw(qW, qX, qY, qZ)
         if not self.x:
-            #self.x.append([rospy.get_time(), phi, point[0], point[1], 0, 0])
             measured_state = np.array([phi, point.x, point.y])
             marray = Float64MultiArray()
             marray.data = [time.perf_counter()] + measured_state.flatten().tolist() + [self.u[0], self.u[1]]
@@ -65,7 +41,6 @@ class turtlebot_ekf():
         noisy_state = np.random.multivariate_normal(measured_state, np.array([[1, 0.5, 0.1], [0.5, 0.1, 0.2], [0.1, 0.2, 0.2]])*0.0001)
         marray.data = [time.perf_counter()] + (noisy_state).flatten().tolist()
         self.last_y = marray
-        #self.y_pub.publish(marray)
 
     def twist_callback(self, msg):
         linear_vel = msg.linear.x
@@ -77,20 +52,16 @@ class turtlebot_ekf():
         self.u[0] = left_wheel_vel
         self.u[1] = right_wheel_vel
         self.last_u = marray
-        #self.u_pub.publish(marray)
 
 
     def publish_states(self, event):
         if self.last_u is not None:
-            print("PUBLISHING U: ", self.last_u)
             self.u_pub.publish(self.last_u)
         
         if self.last_x is not None:
-            print("PUBLISHING X: ", self.last_x)
             self.x_pub.publish(self.last_x)
         
         if self.last_y is not None:
-            print("PUBLISHING Y: ", self.last_y)
             self.y_pub.publish(self.last_y)
     
     def quat_to_yaw(self, q0, q1, q2, q3):
@@ -98,12 +69,6 @@ class turtlebot_ekf():
         return math.atan2(2.0 * (q3 * q0) + (q1 * q2), -1.0 + 2.0 * (q0 * q0 + q1 * q1))
 
 def main():
-    """Entry point of the estimator node.
-
-    Returns
-    -------
-        None
-    """
     ekf = turtlebot_ekf()
     rospy.spin()
     rospy.shutdown()
